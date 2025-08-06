cbe596f87e5fa9822d58be5c4c5560a57d50a23f
Jesse Anderson <jesse.anderson@gmail.com>
2025-08-06 13:05:29 -0700

    feat: write stac to azure

--- src/create_stac_items.py
+++ src/create_stac_items.py
@@ -1,11 +1,14 @@
 """Create stac items for all 1/3 arc-second USGS dems."""
 
-import os
+import json
 from pathlib import Path
 
 import obstore
 import rasterio
 import rio_stac
+import rio_stac.stac
+from pystac import Asset
+from pystac import MediaType
 
 
 def create_stac_items(output_folder: Path) -> None:
@@ -16,20 +19,42 @@ def create_stac_items(output_folder: Path) -> None:
         skip_signature=True,
         region="us-west-2",
     )
+
+    output_store = obstore.store.from_url("az://tw-staging", account_name="tealwaters")
+
     for chunk in store.list(chunk_size=1000):
         for item in chunk:
             s3_url = f"{prefix}/{item['path']}"
             if s3_url.endswith(".tif"):
                 with rasterio.open(s3_url) as src:
-                    stac_item = rio_stac.create_stac_item(src)
-
+                    raster_info = {
+                        "raster:bands": rio_stac.stac.get_raster_info(
+                            src, max_size=1024
+                        )
+                    }
+                    assets = dict(
+                        dem=Asset(
+                            media_type=MediaType.COG,
+                            href=s3_url,
+                            roles=["data"],
+                            extra_fields={**raster_info},
+                        )
+                    )
+                    stac_item = rio_stac.create_stac_item(
+                        src,
+                        assets=assets,
+                        with_proj=True,
+                        with_raster=True,
+                        with_eo=True,
+                        collection="USGS_DEM_13",
+                    )
+                item_json = json.dumps(stac_item.to_dict(), indent=2).encode("utf-8")
                 output_path = output_folder / Path(s3_url).with_suffix(".json").name
-                stac_item.save_object(dest_href=str(output_path))
+                output_store.put(str(output_path), item_json)
                 print(output_path)
 
 
 if __name__ == "__main__":
-    output_folder = Path("data")
-    os.makedirs(output_folder, exist_ok=True)
+    output_folder = Path("usgs/dem/13")
     with rasterio.Env(AWS_NO_SIGN_REQUEST="YES", AWS_REGION="us-west-2"):
         create_stac_items(output_folder)
