5fa2cece1f2ad07bee998ad62526b27af8896c84
Jesse Anderson <jesse.anderson@gmail.com>
2025-08-20 12:24:04 -0700

    feat: set default arguments for

--- src/ingest_to_pgstac.py
+++ src/ingest_to_pgstac.py
@@ -10,58 +10,40 @@ from pypgstac.load import Loader
 from pystac import Collection
 from pystac import Item
 
+CONTAINER = "tw-staging"
+ACCOUNT_NAME = "tealwaters"
 PG_DB = PgstacDB()
 
 app = typer.Typer()
 
 
 @app.command()
-def ingest_collection(url: str, pg_db: PgstacDB = PG_DB) -> None:
+def ingest_collection(url: str) -> None:
     """Ingest a pystac Collection into the given database."""
     collection = Collection.from_file(url)
-    loader = Loader(pg_db)
+    loader = Loader(PG_DB)
     loader.load_collections(iter([collection.to_dict()]))
 
 
 @app.command()
 def load_and_ingest_items(
-    container: str,
     prefix: str,
-    account_name: str,
     collection_url: str,
-    pg_db: PgstacDB = PG_DB,
+    container: str = CONTAINER,
+    account_name: str = ACCOUNT_NAME,
 ) -> None:
-    """Load items in the given container & prefix and ingest into the database.
+    """Load items and ingest into the STAC database.
 
     Args:
-        container:
         prefix:
-        account_name:
         collection_url:
-        pg_db:
+        container:
+        account_name:
 
     """
     items = get_items(container, prefix, account_name)
     collection = Collection.from_file(collection_url)
-    return ingest_items(items, collection, pg_db)
-
-
-def ingest_items(
-    items: list[Item], collection: Collection, pg_db: PgstacDB = PG_DB
-) -> None:
-    """Ingest items into pgstac database."""
-    for item in items:
-        item.collection_id = collection.id
-        item.add_link(
-            pystac.Link(
-                pystac.RelType.COLLECTION,
-                collection.id,
-                media_type=pystac.MediaType.JSON,
-            )
-        )
-
-    loader = Loader(pg_db)
-    loader.load_items(iter([item.to_dict() for item in items]))
+    return ingest_items(items, collection)
 
 
 def get_items(
@@ -95,6 +77,24 @@ def get_items(
     if len(items) == 0:
         raise Exception("No STAC Items found")
     return items
+
+
+def ingest_items(
+    items: list[Item], collection: Collection, pg_db: PgstacDB = PG_DB
+) -> None:
+    """Ingest items into pgstac database."""
+    for item in items:
+        item.collection_id = collection.id
+        item.add_link(
+            pystac.Link(
+                pystac.RelType.COLLECTION,
+                collection.id,
+                media_type=pystac.MediaType.JSON,
+            )
+        )
+
+    loader = Loader(pg_db)
+    loader.load_items(iter([item.to_dict() for item in items]))
 
 
 if __name__ == "__main__":
