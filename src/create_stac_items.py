193e52bda7e6145f8bfa2378243664e524862176
Jesse Anderson <jesse.anderson@gmail.com>
2025-08-28 12:18:18 -0700

    feat:working stac ingestion using eoapi script

--- src/create_stac_items.py
+++ src/create_stac_items.py
@@ -1,6 +1,7 @@
 """Create stac items for all 1/3 arc-second USGS dems."""
 
 import json
+from datetime import datetime
 from io import TextIOWrapper
 from pathlib import Path
 
@@ -10,6 +11,13 @@ import rio_stac
 import rio_stac.stac
 from pystac import Asset
 from pystac import MediaType
+
+# Items should have a standard datetime (So they don't come in as multi-dimensional)
+# Since we are crawling the "current" folder there should only be one version
+# of anything. The alternative might be to record the timestamp of the file,
+# as there is no metadata with the data itself that indicates production time
+# (The xmls seem to give a range of dates in the past from e.g. 1999 to 2013.
+DATETIME = datetime.now()
 
 
 def create_stac_items(output_file: TextIOWrapper) -> None:
@@ -21,7 +29,6 @@ def create_stac_items(output_file: TextIOWrapper) -> None:
         region="us-west-2",
     )
 
-    output_store = obstore.store.from_url("az://tw-staging", account_name="tealwaters")
     output_file.write("[")
 
     for chunk in store.list(chunk_size=1000):
@@ -29,26 +36,30 @@ def create_stac_items(output_file: TextIOWrapper) -> None:
             s3_url = f"{prefix}/{item['path']}"
             if s3_url.endswith(".tif"):
                 with rasterio.open(s3_url) as src:
-                    raster_info = {
-                        "raster:bands": rio_stac.stac.get_raster_info(
-                            src, max_size=1024
-                        )
-                    }
+                    #                    raster_info = {
+                    #                        "raster:bands": rio_stac.stac.get_raster_info(
+                    #                            src, max_size=1024
+                    #                        )
+                    #                    }
+                    https_url = s3_url.replace(
+                        "s3://prd-tnm", "https://prd-tnm.s3.amazonaws.com"
+                    )
                     assets = dict(
                         dem=Asset(
                             media_type=MediaType.COG,
-                            href=s3_url,
+                            href=https_url,
                             roles=["data"],
-                            extra_fields={**raster_info},
+                            # extra_fields={**raster_info},
                         )
                     )
                     stac_item = rio_stac.create_stac_item(
                         src,
+                        input_datetime=DATETIME,
                         assets=assets,
                         with_proj=True,
                         with_raster=True,
                         with_eo=True,
-                        collection="USGS_DEM_13",
+                        collection="usgs_dem_13",
                     )
                 item_json = json.dumps(
                     stac_item.to_dict(), indent=2
